0e03cc59591d037fe2bbc33c9caa1340ec2fa608
0xNotes <notes@sigil.systems>
2022-12-12 00:38:15 -0500

    Updates

--- src/components/NFTSelect.tsx
+++ src/components/NFTSelect.tsx
@@ -2,28 +2,53 @@
 import arrowLeft from "../assets/arrowLeft.png";
 import arrowRight from "../assets/arrowRight.png";
 
+import Miaura from "../assets/milady-aura.jpg";
+import RadBro from "../assets/radbro.jpg";
+import BoredMiladyMaker from "../assets/bored-milady2.jpg";
+import Ghiblady from "../assets/ghiblady.jpg";
+import McLady from "../assets/mclady.jpg";
+import Bitch from "../assets/milady-that-bitch.jpg";
+import Remi from "../assets/rem_baby.jpg";
+
+
+import { useState } from 'react';
+
+
 type Props = {
     backgroundTexture: any;
 
 };
 
 
+
 export default function NFTSelect({
     backgroundTexture,
     }: Props) {
+    
+    const pics = [Miaura, RadBro, BoredMiladyMaker, Ghiblady, McLady, Bitch, Remi];
+
+    //Do a useeffect to handle a list fix if pics < 5 items 
+    const [position, setPosition] = useState(0);
+
+    function numberFix(inp: number){
+        while(inp < 0){
+            inp += pics.length;
+        }
+        return(inp % pics.length)
+    }
+    
+
     return (
         <div className="NFTContainer" style={{ backgroundImage: `url(${backgroundTexture})`, objectFit: "fill" }}>
-            <div className="FeaturedNFT">
-                TEST
-            </div>
+            <img className="FeaturedNFT" src={pics[position]}/>
             <div className="NFTSelector">
-                <img src={arrowLeft}></img>
-                <div className="NFTPreview" />
-                <div className="NFTPreview" />
-                <div className="NFTPreview" />
-                <div className="NFTPreview" />
-                <div className="NFTPreview" />
-                <img src={arrowRight}></img>
+                <img src={arrowLeft} onClick={() => setPosition(numberFix(position-1))}></img>
+                <img className="NFTPreview" src={pics[numberFix(position-2)]}/>
+                <img className="NFTPreview" src={pics[numberFix(position-1)]}/>
+                <img className="NFTCenter" src={pics[numberFix(position)]}/>
+                <img className="NFTPreview" src={pics[numberFix(position+1)]}/>
+                <img className="NFTPreview" src={pics[numberFix(position+2)]}/>
+                <img src={arrowRight} onClick={() => setPosition(numberFix(position+1))}></img>
 
             </div>
         </div>
